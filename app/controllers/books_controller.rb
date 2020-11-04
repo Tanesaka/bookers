6079e3f62cb74ce798daecac12b27929796a3db6
Tanesaka <seedtanesa@outlook.jp>
2020-11-04 20:29:48 +0000

    [Update]3-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,35 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+  end
+
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    #下記のリンクもas: bookってやったやつ
+    redirect_to book_path(book.id)
+  end
+
+
+  def edit
+  end
+
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'books/index' => 'books#index'
+
+  get 'books/new'
+  post 'books/new' => 'books#create'
+
+  get 'books/edit'
+
+  get 'books/:id' => 'books#show', as: 'book'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
