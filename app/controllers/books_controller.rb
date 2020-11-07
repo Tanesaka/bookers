f872daf185e591b0e8e50c327ce96fb2dbd88d47
Tanesaka <seedtanesa@outlook.jp>
2020-11-07 10:28:11 +0000

    [Finish]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,13 @@
 class BooksController < ApplicationController
 
   def create
-    book = Book.new(book_params)
-    book.save
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
     #下記のリンクもas: bookってやったやつ
-    redirect_to book_path(book.id), notice:'Book was successfully created.'
+    redirect_to book_path(@book.id), notice:'Book was successfully created.'
+    else render :index
+    end
   end
 
   def index
@@ -22,9 +25,11 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
     redirect_to book_path(book_params), notice:'Book was successfully updated.'
+    else render :edit
+    end
   end
 
   def destroy
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,12 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
-
-# 空欄だと保存されないようにする
-
-class Book < ApplicationRecord
-  validates :title, presence: true
-  validates :body, presence: true
-end
-
-# ここまで
